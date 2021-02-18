6cf86946dec060fbcbc0558e86df8b9b48dabc7f
N00Binho <brunocamargomartins21@gmail.com>
2021-02-18 12:10:35 -0300

    Forca finalizada

---
+++ forca.py
@@ -0,0 +1,63 @@
+# palavra_secreta = ['M', 'A', 'N', 'T', 'E', 'I', 'G', 'A']
+
+print('\n***Jogo da Forca***')
+
+palavra = str(input('Digite uma palavra para a forca\n')).strip().upper()
+palavra_secreta = list(palavra)
+
+letras_descobertas = []
+
+print('\n\n\n\n\n\n\n\n\n\n')
+
+for i in range(0, len(palavra_secreta)):
+    letras_descobertas.append('_')
+
+acertou = False
+contador = 0
+
+while acertou == False:
+    letra_escrita = str(input('Digite uma letra\n')).strip().upper()
+
+    if len(letra_escrita) >= 2 or len(letra_escrita) <= 0:
+        print('Letras demais ou nada escrito')
+    else:
+        contador += 1
+        for i in range(0, len(palavra_secreta)):
+            if letra_escrita == palavra_secreta[i]:
+                contador -= 1
+                letras_descobertas[i] = letra_escrita
+
+        acertou = True
+
+        for x in range(0, len(letras_descobertas)):
+            if letras_descobertas[x] == '_':
+                acertou = False
+
+        print(letras_descobertas)  # catchau
+
+        if contador == 7:
+            print("""
+                 uuuuuuu
+             uu$$$$$$$$$$$uu
+          uu$$$$$$$$$$$$$$$$$uu
+         u$$$$$$$$$$$$$$$$$$$$$u
+        u$$$$$$$$$$$$$$$$$$$$$$$u
+       u$$$$$$$$$$$$$$$$$$$$$$$$$u
+       u$$$$$$$$$$$$$$$$$$$$$$$$$u
+       u$$$$$$"   "$$$"   "$$$$$$u
+       "$$$$"      u$u       $$$$"
+        $$$u       u$u       u$$$
+        $$$u      u$$$u      u$$$
+         "$$$$uu$$$   $$$uu$$$$"
+          "$$$$$$$"   "$$$$$$$"
+            u$$$$$$$u$$$$$$$u
+             u$"$"$"$"$"$"$u
+             $$u$ $ $ $ $u$$       
+              $$$$$u$u$u$$$       
+               "$$$$$$$$$" 
+               
+              Você Perdeu!!!""")
+            break
+
+        if acertou:
+            print('VOCÊ GANHOU!!!!!!')
